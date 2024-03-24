97d673e2cf218d846fc7ed98c8823be74e3dc3cd
MilkteaTW1 <hankyourty@gmail.com>
2024-03-24 16:08:54 +0800

    p

--- nowbie.py
+++ nowbie.py
@@ -14,4 +14,12 @@ url = "https://ssr1.scrape.center/"
 response = requests.get(url=url,headers=header)
 
 soup = BeautifulSoup(response.text, 'html.parser')
-result = soup.find_all(name='div')
+#解析Html訊息
+
+result = soup.find_all(name='div' , class_='el-card item m-t is-hover-shadow')
+#尋找div標籤含有class=el-card item m-t is-hover-shadow的
+
+for i in range(len(result)):
+    n=i-1
+    print(result[n].h2.string)
+#列出所有電影名稱
